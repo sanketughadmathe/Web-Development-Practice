0efcff3cf31000cd23fdb40512fa0442f3d26dca
Sanket <sanketughadmathe@gmail.com>
2022-12-27 19:14:14 +0530

    Code changes

--- Drum/index.js
+++ Drum/index.js
@@ -1,1 +1,9 @@
-alert("Hi!");
+var audio = new Audio('sounds/crash.mp3');
+audio.play();
+
+for (let index = 0; index < document.querySelectorAll(".drum").length; index++) {
+  const element = document.querySelectorAll(".drum")[index];
+  document.querySelectorAll(".drum")[index].addEventListener("click", function () {
+    audio.play();
+  })
+}
